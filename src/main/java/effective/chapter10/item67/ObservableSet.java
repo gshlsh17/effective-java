0003479835c6aef77a8a08a31954687beebb47e6
lee2seek <llxjtu08@gmail.com>
2017-07-25 11:38:06 +0800

    compleete item67

--- src/main/java/effective/chapter10/item67/ObservableSet.java
+++ src/main/java/effective/chapter10/item67/ObservableSet.java
@@ -3,8 +3,12 @@ package effective.chapter10.item67;
 import effective.chapter4.item16.ForwardingSet;
 
 import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
+ * 可以使用CopyOnWriteArrayList类型代替observers，就能避免异常和死锁的发生
  * @author ll
  * @create 2017-07-24
  */
@@ -55,16 +59,46 @@ public class ObservableSet<E> extends ForwardingSet<E> {
 
     public static void main(String[] args) {
         ObservableSet<Integer> set = new ObservableSet<>(new HashSet<>());
+//        set.addObserver(new SetObserver<Integer>() {
+//            @Override
+//            public void added(ObservableSet set, Integer element) {
+//                System.out.println(element);
+//            }
+//        });
+
+        // 会报ConcurrentModificationException异常，因为在add操作时，会遍历set，而遍历的过程中等于23时删除观察者，所以报错
+//        set.addObserver(new SetObserver<Integer>() {
+//            @Override
+//            public void added(ObservableSet set, Integer element) {
+//                System.out.println(element);
+//                if (element == 23) {
+//                    set.removeObserver(this);
+//                }
+//            }
+//        });
+        // 下面这种方式会造成死锁
         set.addObserver(new SetObserver<Integer>() {
             @Override
-            public void added(ObservableSet set, Integer element) {
+            public void added(final ObservableSet set, Integer element) {
                 System.out.println(element);
-            }
-        });
-        set.addObserver(new SetObserver<Integer>() {
-            @Override
-            public void added(ObservableSet set, Integer element) {
-                System.out.println("我是第二个观察者");
+                if (element == 23) {
+                    ExecutorService executor = Executors.newSingleThreadExecutor();
+                    final  SetObserver<Integer> observer = this;
+                    try {
+                        executor.submit(new Runnable() {
+                            @Override
+                            public void run() {
+                                set.removeObserver(observer);
+                            }
+                        }).get();
+                    } catch (ExecutionException e) {
+                        throw new AssertionError(e.getCause());
+                    } catch (InterruptedException e) {
+                        throw new AssertionError(e.getCause());
+                    } finally {
+                        executor.shutdown();
+                    }
+                }
             }
         });
         for (int i = 0; i < 100; i++) {
