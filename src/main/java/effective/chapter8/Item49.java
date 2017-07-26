ae511176d606dbca2c7beaf6effdf1004b59a39e
lee2seek <llxjtu08@gmail.com>
2017-07-26 17:19:54 +0800

    modify Item49

--- src/main/java/effective/chapter8/Item49.java
+++ src/main/java/effective/chapter8/Item49.java
@@ -3,6 +3,7 @@ package effective.chapter8;
 import java.util.Comparator;
 
 /**
+ *
  * @author ll
  * @create 2017-07-26
  */
@@ -10,7 +11,7 @@ public class Item49 {
     Comparator<Integer> badComparator = new Comparator<Integer>() {
         @Override
         public int compare(Integer o1, Integer o2) {
-            // 实际运行时，这里会将o1和o2抓为对应的int，之后进行比较，但是effective java书中是进行对象的比较
+            // 实际运行时（JDK8_141），这里会将o1和o2转为对应的int，之后进行比较，但是effective java书中是进行对象的比较
             return o1 < o2 ? -1 : (o1 == o1 ? 0 : 1);
         }
     };
@@ -24,9 +25,15 @@ public class Item49 {
     };
 
     public static void main(String[] args) {
+        Integer temp1 = 42;
+        Integer temp2 = 42;
+        // 输出true，因为Integer对于[-128,127]之间的数字进行了缓存
+        System.out.println(temp1 == temp2);
+
         Integer o1 = new Integer(42);
         Integer o2 = new Integer(42);
         System.out.println(o1 == o2);
+
         Item49 item49 = new Item49();
         System.out.println("调用badComparator比较结果：" + item49.badComparator.compare(o1, o2));
         System.out.println("调用goodComparator比较结果：" + item49.goodComparator.compare(o1, o2));
