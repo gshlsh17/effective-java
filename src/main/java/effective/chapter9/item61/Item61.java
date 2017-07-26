01fe7208004f615021f30cfb8e1a76261131d30e
lee2seek <llxjtu08@gmail.com>
2017-07-26 11:29:06 +0800

    commit item61

--- src/main/java/effective/chapter9/item61/Item61.java
+++ src/main/java/effective/chapter9/item61/Item61.java
@@ -1,14 +1,12 @@
 package effective.chapter9.item61;
 
-import java.util.AbstractSequentialList;
-import java.util.List;
-
 /**
  * @author ll
  * @create 2017-07-26
  */
 public class Item61 {
     public static void main(String[] args) {
-        AbstractSequentialList
+        //AbstractSequentialList 中的get方法使用了异常转移
+//        AbstractSequentialList
     }
 }
