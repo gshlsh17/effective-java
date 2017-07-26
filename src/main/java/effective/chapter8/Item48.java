549280b94f2ce40cc7f38109008090eaabf666aa
lee2seek <llxjtu08@gmail.com>
2017-07-26 17:12:54 +0800

    item48,item49,chapter9

--- src/main/java/effective/chapter8/Item48.java
+++ src/main/java/effective/chapter8/Item48.java
@@ -1,8 +1,43 @@
 package effective.chapter8;
 
+import java.math.BigDecimal;
+
 /**
+ *
  * @author ll
  * @create 2017-07-26
  */
 public class Item48 {
+    /**
+     * 采用double实现，发现计算不准确
+     */
+    public static void compute1() {
+        double funds = 1.00;
+        int itemsBought = 0;
+        for (double price = .10; funds >= price; price += .10) {
+            funds -= price;
+            itemsBought++;
+        }
+        System.out.println("itemsBought:" + itemsBought);
+        System.out.println("剩余的钱数：" + funds);
+    }
+
+    /**
+     * 采用BigDecimal实现，准确计算数字
+     */
+    public static void compute2() {
+        final BigDecimal TEN_CENTS = new BigDecimal(".10");
+        int itemsBought = 0;
+        BigDecimal funds = new BigDecimal("1.00");
+        for (BigDecimal price = TEN_CENTS; funds.compareTo(price) >= 0; price = price.add(TEN_CENTS)) {
+            itemsBought++;
+            funds = funds.subtract(price);
+        }
+        System.out.println("itemsBought:" + itemsBought);
+        System.out.println("剩余的钱："+funds);
+    }
+    public static void main(String[] args) {
+        Item48.compute1();
+        Item48.compute2();
+    }
 }
--- src/main/java/effective/chapter8/Item49.java
+++ src/main/java/effective/chapter8/Item49.java
@@ -1,8 +1,34 @@
 package effective.chapter8;
 
+import java.util.Comparator;
+
 /**
  * @author ll
  * @create 2017-07-26
  */
 public class Item49 {
+    Comparator<Integer> badComparator = new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            // 实际运行时，这里会将o1和o2抓为对应的int，之后进行比较，但是effective java书中是进行对象的比较
+            return o1 < o2 ? -1 : (o1 == o1 ? 0 : 1);
+        }
+    };
+    Comparator<Integer> goodComparator = new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            int i1 = o1.intValue();
+            int i2 = o2.intValue();
+            return i1 < i2 ? -1 : (i1 == i2 ? 0 : 1);
+        }
+    };
+
+    public static void main(String[] args) {
+        Integer o1 = new Integer(42);
+        Integer o2 = new Integer(42);
+        System.out.println(o1 == o2);
+        Item49 item49 = new Item49();
+        System.out.println("调用badComparator比较结果：" + item49.badComparator.compare(o1, o2));
+        System.out.println("调用goodComparator比较结果：" + item49.goodComparator.compare(o1, o2));
+    }
 }
